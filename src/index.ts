ea3609170670ec42197c5e5dc755505edd5b1ce5
Garrison <garrison@hey.com>
2022-09-09 16:45:05 -0500

    extension

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { window, workspace, RelativePattern } from 'vscode'
+import { readFile, writeFile } from 'fs';
+import { window, workspace, RelativePattern, DebugConsoleMode } from 'vscode'
 
 
 
 export function activate() {
-  window.showInformationMessage('hello')
+  console.log(124)
   const folder = workspace.workspaceFolders?.[0];
   if (folder) {
     const watcher = workspace.createFileSystemWatcher(new RelativePattern(folder, '**.*'));
 
     watcher.onDidChange((e) => {
-      console.log(e)
+      readFile(e.path, (error, data) => {
+        const file = data.toString();
+        // persist to indexdb
+      })
     })
   }
 }
