875094a738c7d6aa46b2be481687327dad6ecee7
HeyGarrison <garrison@hey.com>
2022-09-09 16:10:29 -0500

    help me

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { window, workspace, RelativePattern } from 'vscode'
 
 
 export function activate() {
+  window.showInformationMessage('hello')
   const folder = workspace.workspaceFolders?.[0];
   if (folder) {
     const watcher = workspace.createFileSystemWatcher(new RelativePattern(folder, '**.*'));
