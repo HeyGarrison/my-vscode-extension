866718db5fc226382e5149c82be0fb0ab256ee2e
HeyGarrison <garrison@hey.com>
2022-09-12 13:52:09 -0500

    try this

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,7 @@ import { window, workspace, RelativePattern, DebugConsoleMode } from 'vscode'
 
 
 export function activate() {
-  console.log(124)
-  const folder = workspace.workspaceFolders?.[0];
-  if (folder) {
-    const watcher = workspace.createFileSystemWatcher(new RelativePattern(folder, '**.*'));
-
-    watcher.onDidChange((e) => {
-      readFile(e.path, (error, data) => {
-        const file = data.toString();
-        // persist to indexdb
-      })
-    })
-  }
+  localStorage['hello'] = 1
 }
 
 export function deactivate() {
