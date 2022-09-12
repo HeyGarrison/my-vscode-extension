c04b80e272fc220c7650af59e33ac58db551a2d7
HeyGarrison <garrison@hey.com>
2022-09-12 14:19:25 -0500

    come on

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import { readFile, writeFile } from 'fs';
+import { LocalStorageService } from './localStorageService';
+import {ExtensionContext} from 'vscode'
 
+interface ISomeObject {
+  hello: boolean
+}
 
+export function activate(context: ExtensionContext): any {
 
-export function activate() {
-  console.log('does this work')
-  window['localStorage']['hello'] = 'yes'
-  console.log(window['localStorage']['hello']);
+  //Create your objects - Needs to be a well-formed JSON object.
+  let someObject : ISomeObject = {hello: true}
+  let someOtherObject : ISomeObject | null = null;
+
+  //Initialize the global application manager
+  let storageManager = new LocalStorageService(context.workspaceState);
+
+  //Read your objects to the Workspace Store
+  someOtherObject = storageManager.getValue<ISomeObject>("SomeObject");
+  console.log(someOtherObject)
+
+  //Write your objects from the Workspace Store
+  storageManager.setValue<ISomeObject>("SomeObject", someObject);
+
+  //Read your objects to the Workspace Store
+  console.log(someOtherObject)
 }
 
 export function deactivate() {
